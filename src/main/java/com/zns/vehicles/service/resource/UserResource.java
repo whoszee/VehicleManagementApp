79de31a66203dfcab21629e9e996b39904d07131
whoszee <mzeeshan45@gmail.com>
2016-07-26 11:52:01 -0500

    added additional resources

--- src/main/java/com/zns/vehicles/service/resource/UserResource.java
+++ src/main/java/com/zns/vehicles/service/resource/UserResource.java
@@ -2,13 +2,14 @@ package com.zns.vehicles.service.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 
 
 @Path("/user")
-public class UserService {
+public class UserResource {
 	
 	@Path("/hello")
 	@GET
@@ -30,5 +31,21 @@ public class UserService {
 	public String getUsersWparam(@Context UriInfo info)  {
 		String param = info.getQueryParameters().getFirst("name");
 		return "the name is "+param;
+	}
+	
+	@Path("/{username}")
+	@GET
+	@Produces("text/html")
+	public String getUser(@PathParam("username") String userName)  {
+		
+		return "Welcome to "+userName+"'s profile page...";
+	}
+	
+	@Path("/{username}/vehicles")
+	@GET
+	@Produces("text/html")
+	public String getUserVehicles(@PathParam("username") String userName)  {
+		
+		return "Welcome to "+userName+"'s vehicle listing page...";
 	}
 }
